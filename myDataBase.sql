86f34ead7c323296cd8b49f9e467721d9ce89a8d
fr0st99 <48594888+fr0st99@users.noreply.github.com>
2021-11-02 18:09:53 +0000

    Add files via upload
    Simple database to test functionality of Users.java and Login.java.
    
    Needs to be uploaded to AWS or online host of some kind

---
+++ myDataBase.sql
@@ -0,0 +1,5 @@
+create database myDatabase;
+use myDatabase;
+create table student(USERNAME varchar(30) not null,UNISTATUS varchar(10) ,
+CONFIRMPASSWRD varchar(20) ,CITY varchar(30),
+EMAIL varchar(30) );
