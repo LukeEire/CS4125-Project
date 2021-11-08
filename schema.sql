a3fc97fbf1208736b038d272820ea45db6d23d7e
Ayoub <ayoub324@icloud.com>
2021-11-08 21:57:55 +0000

    New Schema design

---
+++ schema.sql
@@ -0,0 +1,20 @@
+create database myDatabase;
+use myDatabase;
+create table student(USERNAME varchar(30) not null,UNISTATUS varchar(10) ,
+CONFIRMPASSWRD varchar(20) ,CITY varchar(30),
+EMAIL varchar(30) );
+
+CREATE TABLE USERS IF NOT EXIST(
+id CHAR(8) PRIMARY KEY,
+firstName varchar(255) NOT NULL,
+lastName varchar(255) NOT NULL,
+password VARCHAR(255),
+email_address VARCHAR(320),
+status VARCHAR(32),
+electric TINYINT(1),
+penalties int(11),
+ban_status TINYINT(1),
+accessibility TINYINT(1),
+created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+dob DATE
+);
