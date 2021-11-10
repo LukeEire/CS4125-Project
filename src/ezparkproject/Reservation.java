85307e6a05adaa9e3660d4ccda0d8c6c4a397c27
LukeEire <18250785@studentmail.ul.ie>
2021-11-10 15:04:42 +0000

    Started tweaking reservation to meet new standard but needs more work

--- src/ezparkproject/Reservation.java
+++ src/ezparkproject/Reservation.java
@@ -1,5 +1,7 @@
 package ezparkproject;
 import ezparkproject.Users;
+
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.Period;
 
@@ -9,34 +11,26 @@ public class Reservation {
 	
     private Users user;
     //private Lot lot ;
-    private Spaces space;
+    //private Spaces space;
     int duration; //in minutes
     LocalDateTime checkInDate;
     LocalDateTime checkOutDate;
 
 
-    public Reservation(Users user, Spaces space, int duration) {
+    public Reservation(Users user, int duration) {
         this.user = user;
-        this.space = space;
+        //this.space = space;
         this.duration = duration;
         checkInDate = LocalDateTime.now();
         checkOutDate = checkInDate.plusMinutes(duration);
     }
 
-    public User getUser() {
+    public Users getUser() {
         return user;
     }
 
     public void setUser(Users user) {
         this.user = user;
-    }
-
-    public Spaces getSpaces() {
-        return space;
-    }
-
-    public void setSpaces(Spaces space) {
-        this.space = space;
     }
 
     public LocalDateTime getcheckInDate() {
