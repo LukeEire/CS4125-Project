9793cb150dfd7e5370359c611c4c156873cbe1db
LukeEire <18250785@studentmail.ul.ie>
2021-11-11 20:25:10 +0000

    Started work on main screen for managing bookings etc

--- src/ezparkproject/UiFrame.java
+++ src/ezparkproject/UiFrame.java
@@ -9,4 +9,54 @@ public class UiFrame implements ActionListener{
 
 	JFrame frame;
 	
+	
+	
+	/* Buttons */
+	JButton newBookButton = new JButton("New Booking");
+	JButton cancelBookButton = new JButton("Cancel Booking");
+	JButton exitButton = new JButton("Exit");
+	
+	
+	
+	public void actionEvent() {
+
+		newBookButton.addActionListener(this);
+		cancelBookButton.addActionListener(this);
+		exitButton.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+
+		/* Create Database before using */
+
+		if (e.getSource() == newBookButton) {
+
+			try {
+				
+				/* Create Connection Object */
+				Connection con = DriverManager.getConnection("jdbc:mysql://sql4.freesqldatabase.com:3306/sql4450358","sql4450358","dcCxqbDW1K");
+				
+				/* Pass values into Database */
+				PreparedStatement Pstatement = con.prepareStatement("insert into Register values(?,?,?,?,?,?,?,?,?)");
+			
+
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+
+		}
+			
+		
+		if (e.getSource() == cancelBookButton) {
+			
+		}
+
+		if (e.getSource() == exitButton) {
+
+			/* Close current window */
+			frame.dispose();
+	        
+		}
+	}
+	
 }
