9bec54f5f084bb930198ee9a25347ab1349781b0
fr0st99 <48594888+fr0st99@users.noreply.github.com>
2021-11-12 15:33:59 +0000

    Added logo to login page and register button. Resized windows and text

--- src/ezparkproject/LoginFunction.java
+++ src/ezparkproject/LoginFunction.java
@@ -3,6 +3,7 @@ package ezparkproject;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -11,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,8 +27,9 @@ public class LoginFunction extends JFrame {
     private static final long serialVersionUID = 1L;
     private JTextField textField;
     private JPasswordField passwordField;
-    private JButton btnNewButton;
+    private JButton loginButton;
     private JButton logoutButton;
+    private JButton registerButton;
     private JLabel label;
     private JPanel contentPane;
 
@@ -52,18 +55,30 @@ public class LoginFunction extends JFrame {
     public LoginFunction() {
     	
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(450, 190, 1014, 597);
-        setResizable(false);
+        setBounds(500, 200, 1100, 600);
+        setResizable(true);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
+        
+        /* Adding an Image logo to the start of the login screen */
+        
+        label = new JLabel("");
+        Image img = new ImageIcon(this.getClass().getResource("logo.png")).getImage();
+        label.setIcon(new ImageIcon(img));
+        label.setBounds(503, 13, 273, 93);
+        label.setSize(100,100);
+        contentPane.add(label);
+        
+        
+        /* Placeholder login instead of logo below */
 
-        JLabel lblNewLabel = new JLabel("Login");
+       /*JLabel lblNewLabel = new JLabel("Login");
         lblNewLabel.setForeground(Color.BLACK);
         lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 46));
         lblNewLabel.setBounds(503, 13, 273, 93);
-        contentPane.add(lblNewLabel);
+        contentPane.add(lblNewLabel); */
 
         textField = new JTextField();
         textField.setFont(new Font("Tahoma", Font.PLAIN, 32));
@@ -79,21 +94,22 @@ public class LoginFunction extends JFrame {
         JLabel lblUsername = new JLabel("Your ID Number");
         lblUsername.setBackground(Color.BLACK);
         lblUsername.setForeground(Color.BLACK);
-        lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        lblUsername.setBounds(250, 166, 193, 52);
+        lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        lblUsername.setBounds(300, 166, 193, 52);
         contentPane.add(lblUsername);
 
         JLabel lblPassword = new JLabel("Password");
         lblPassword.setForeground(Color.BLACK);
-        lblPassword.setBackground(Color.CYAN);
-        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
-        lblPassword.setBounds(250, 286, 193, 52);
+        lblPassword.setBackground(Color.BLACK);
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        lblPassword.setBounds(350, 286, 193, 52);
         contentPane.add(lblPassword);
 
-        btnNewButton = new JButton("Login");
-        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
-        btnNewButton.setBounds(545, 392, 162, 73);
-        btnNewButton.addActionListener(new ActionListener() {
+        loginButton = new JButton("Login");
+        loginButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        loginButton.setBounds(300, 392, 162, 73);
+        loginButton.setSize(150,50);
+        loginButton.addActionListener(new ActionListener() {
 
         	public void actionPerformed(ActionEvent e) {
                 String userID = textField.getText();
@@ -112,9 +128,9 @@ public class LoginFunction extends JFrame {
                         Dashboard ah = new Dashboard();
                         ah.setTitle("Welcome");
                         ah.setVisible(true);
-                        JOptionPane.showMessageDialog(btnNewButton, "You have successfully logged in");
+                        JOptionPane.showMessageDialog(loginButton, "You have successfully logged in");
                     } else {
-                        JOptionPane.showMessageDialog(btnNewButton, "Wrong Username & Password");
+                        JOptionPane.showMessageDialog(loginButton, "Wrong Username & Password");
                     }
                 } catch (SQLException sqlException) {
                     sqlException.printStackTrace();
@@ -123,18 +139,33 @@ public class LoginFunction extends JFrame {
         });
         
         logoutButton = new JButton("Quit");
-        logoutButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
-        logoutButton.setBounds(970, 2, 200, 73);
-        logoutButton.setSize(100,50);
+        logoutButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        logoutButton.setBounds(700, 392, 162, 73);
+        logoutButton.setSize(150,50);
         logoutButton.addActionListener(new ActionListener() {
 
         	public void actionPerformed(ActionEvent e) {
         		System.exit(0);
             }
         });
+        
+        registerButton = new JButton("Register");
+        registerButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        registerButton.setBounds(500, 392, 162, 73);;
+        registerButton.setSize(150,50);
+        registerButton.addActionListener(new ActionListener() {
 
-        contentPane.add(btnNewButton);
+        	public void actionPerformed(ActionEvent e) {
+        		
+        		dispose();
+        		new RegFrame();
+        		
+            }
+        });
+
+        contentPane.add(loginButton);
         contentPane.add(logoutButton);
+        contentPane.add(registerButton);
     }
 }
 
