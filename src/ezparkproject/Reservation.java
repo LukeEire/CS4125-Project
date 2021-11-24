8e92fc77e1d41c202f6ff4725a2646ac5dd5f46f
Ayoub <ayoub324@icloud.com>
2021-11-24 21:25:21 +0000

    Ayoub - Updated Resrvations class - new params + constructor

--- src/ezparkproject/Reservation.java
+++ src/ezparkproject/Reservation.java
@@ -2,7 +2,9 @@ package ezparkproject;
 import ezparkproject.Users;
 
 import java.sql.Date;
+import java.sql.Time;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Period;
 import java.util.Calendar;
 
@@ -13,18 +15,45 @@ public class Reservation {
     private Users user;
     //private Lot lot ;
     //private Spaces space;
-    int duration; //in minutes
+    //int duration; //in minutes
     Date checkInDate;
     Date checkOutDate;
 
+    private String lot;
+    private String reg;
+    private LocalDateTime expiry;
+    private LocalDateTime reservationTime;
+    private LocalTime duration;
+    private int accessibility;
+    private int electric;
+    private int id;
+    Database db = new Database();
 
-    public Reservation(Users user, int duration) {
+    // public Reservation(Users user, int duration) {
+    //     this.user = user;
+    //     //this.space = space;
+    //     this.duration = duration;
+    //     //Review these two lines of code
+    //     checkInDate = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+    //     //checkOutDate = checkInDate.plusMinutes(duration);
+    // }
+
+    // Ayoub - new Reservation constructor
+    // Adds reservation to DB
+    public Reservation(Users user, String lot, /* has to be of format HH:mm */String sduration) {
+        
         this.user = user;
-        //this.space = space;
-        this.duration = duration;
-        //Review these two lines of code
-        checkInDate = new java.sql.Date(Calendar.getInstance().getTime().getTime());
-        //checkOutDate = checkInDate.plusMinutes(duration);
+        this.id = user.id;
+        this.accessibility = user.accessibility;
+        this.electric = user.electric;
+        this.reg = user.getDefultPlate();
+        this.reservationTime = LocalDateTime.now();
+        this.duration = LocalTime.parse(sduration);
+        System.out.println("Testing time: Duration = " + duration );
+        this.expiry = reservationTime.plus(duration);
+        
+        db.reserve(id, reg, lot, electric, accessibility, expiry.toString());
+        
     }
 
     public Users getUser() {
