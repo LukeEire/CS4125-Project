a06b7dc3266c27eabc84183c62356ef048def441
fr0st99 <gamertech109@gmail.com>
2021-11-26 18:46:52 +0000

    Changed main function to run loginFrame first instead of register
    + Various changes to button actions

--- src/ezparkproject/Main.java
+++ src/ezparkproject/Main.java
@@ -2,6 +2,8 @@ package ezparkproject;
 
 import java.sql.SQLException;
 
+import javax.swing.JFrame;
+
 /* Author: Ashutosh Yadav 
  * Student ID: 18249094
  * Date: 02/11/2021
@@ -13,7 +15,12 @@ public class Main {
     public static void main(String[] args) throws Exception
     {
 
-    	new RegFrame();
+    	LoginFunction frame = new LoginFunction();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(500, 200, 1100, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
        
     }
 }
