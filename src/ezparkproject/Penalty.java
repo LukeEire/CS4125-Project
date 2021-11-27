ca1325698c05778356298bccf8eaed236f325d25
Ayoub <ayoub324@icloud.com>
2021-11-27 01:12:56 +0000

    Ayoub - Fixed warnings in penalty class

--- src/ezparkproject/Penalty.java
+++ src/ezparkproject/Penalty.java
@@ -1,8 +1,8 @@
 package ezparkproject;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+// import java.sql.Connection;
+// import java.sql.PreparedStatement;
+// import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Penalty {
