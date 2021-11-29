526bb7d5fcc01f0f4950845131f7831d751527f1
Ayoub <ayoub324@icloud.com>
2021-11-29 00:52:10 +0000

    Ayoub - completed automated testing class for database

--- src/ezparkproject/DatabaseTesting.java
+++ src/ezparkproject/DatabaseTesting.java
@@ -1,4 +1,24 @@
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeEach;
 package ezparkproject;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class DatabaseTest.
+ *
+ * @author  ayoub
+ * @version 28/11/21
+ */
+public class DatabaseTesting
+{
+    /**
+     * Default constructor for test class DatabaseTest
+     */
+    @Test
+     public DatabaseTesting(){
+        package ezparkproject;
 
 public class Testing_database {
     public static void main(String[] args) throws Exception
@@ -13,13 +33,20 @@ public class Testing_database {
 		// HOW TO GET A CON OBJECT (ALTHOUGH YOU DONT REALLY NEED TO)
 		db.connect();
 
-		db.dropTable("reservations");
+        // Clear table contents
+		db.clearTable("users");
+		db.clearTable("reservations");
+		db.clearTable("transactions");
+        
+        // Drop DB tables
 		db.dropTable("users");
+		db.dropTable("reservations");
 		db.dropTable("transactions");
 
+        // CREATE DB tables
+		db.createUsersTable();
 		db.createReservationsTable();
 		db.createTransactionsTable();
-		db.createUsersTable();
 
 		// HOW TO REGISTER A NEW USER
 		db.newUser(18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", "222LH1445");
@@ -29,20 +56,17 @@ public class Testing_database {
 
 		// HOW TO DELETE/BAN/UNBAN A USER USING USER ID'S
 
-		db.deleteUser(18266401);
-		db.deleteUser(18266402);
 		db.deleteUser(18266403);
 		db.deleteUser(18266404);
-		
-		db.banUser(18266403);
-		db.banUser(18266404);
-		
-		db.unBanUser(18266403);
-		db.unBanUser(18266404);
+
+		db.banUser(18266401);
+		db.banUser(18266402);
+
+		db.unBanUser(18266401);
 		
 		
 		// HOW TO PRINT USER'S DB ITEMS - PRINT STATEMENT + ARRAYLIST
-		db.fetchData();
+        db.fetchData();
 		
 		// HOW TO VERIFY A USER (LOGIN)
 		String password = "Password123";
@@ -62,9 +86,14 @@ public class Testing_database {
 		db.fetchUserReservation(ayoub);
 
 		//Transaction
-
 		db.addTransaction(18266401, 1, "Lot A", 2.40);
-		db.addTransaction(18266401, 2, "Lot A", 2.40);
+		db.addTransaction(18266401, 2, "Lot B", 2.45);
+		
+		db.fetchTransactionData();
+		db.fetchUserTransactionData(ayoub.getID());
 
+    }
+}
+        
     }
 }
