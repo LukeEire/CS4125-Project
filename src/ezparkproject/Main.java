03513867dffadc75058e1be78442a712cc7762c4
LukeEire <18250785@studentmail.ul.ie>
2021-11-29 20:34:42 +0000

    Added current user functionality to Main

--- src/ezparkproject/Main.java
+++ src/ezparkproject/Main.java
@@ -8,16 +8,19 @@ import javax.swing.JFrame;
 
 public class Main {
 	
-	Lot LotA = new Lot ("LotA", 300, 10, 3);
-	Lot LotB = new Lot ("LotB", 350, 15, 0);
-	Lot LotC = new Lot ("LotC", 300, 15, 5);
-	Lot LotD = new Lot ("LotD", 400, 20, 10);
+	static Users currentUser = new Users();
+	
+	//These are static as they will need to be consistent through all pages of the program
+	static Lot LotA = new Lot ("LotA", 300, 10, 3);
+	static Lot LotB = new Lot ("LotB", 350, 15, 0);
+	static Lot LotC = new Lot ("LotC", 300, 15, 5);
+	static Lot LotD = new Lot ("LotD", 400, 20, 10);
 	
 	
     public static void main(String[] args) throws Exception
     {
-
-   	LoginFunction frame = new LoginFunction();
+    	
+       LoginFunction frame = new LoginFunction();
        frame.setTitle("Login Form");
        frame.setVisible(true);
        frame.setBounds(500, 200, 1100, 600);
