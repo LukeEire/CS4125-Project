07b2451b618de91a298f96e8e4b64de74f44abf0
Ayoub <ayoub324@icloud.com>
2021-11-29 01:40:43 +0000

    Ayoub - updated testing statements

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ create table student(USERNAME varchar(30) not null,UNISTATUS varchar(10) ,
 CONFIRMPASSWRD varchar(20) ,CITY varchar(30),
 EMAIL varchar(30) );
 
---Ayoub Changes
+--Ayoub new users DB
 CREATE TABLE IF NOT EXISTS users (
 id int(32) PRIMARY KEY,
 firstName VARCHAR(255) NOT NULL,
@@ -22,7 +22,23 @@ dob DATE,
 reg VARCHAR(255)
 );
 
---Ayoub - new reservations class
+-- Testing SQL statments
+INSERT INTO users (id, firstName, LastName, password, email_address, status, electric, penalties, ban_status, accessibility, created_on, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", "222LH1445");
+
+INSERT INTO users (id, firstName, LastName, password, status, electric accessibility, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", "222LH1445");
+
+INSERT INTO users (id, firstName, LastName, password, status, electric, accessibility, created_on, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", DATE '2015-12-17', "222LH1445");
+
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `password`, `email_address`, `status`, `electric`, `penalties`, `ban_status`, `banTime`, `accessibility`, `created_on`, `dob`, `reg`) VALUES ('', '', '', MD5(''), NULL, NULL, NULL, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL, NULL);
+
+UPDATE users SET ban_status = 1, banTime = DATE '2000-03-12' WHERE id = "18266401"
+
+INSERT INTO users (id, firstName, LastName, password, status, electric, accessibility, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", "222LH1445");
+
+SELECT * FROM users WHERE id = 18173581 AND password =  MD5('test123')
+
+SELECT * FROM users WHERE id = 18266401 AND password = "Password123"
+--Ayoub - new reservations DB
 -- // Preliminary List of booking requirements (for reservations table): 
 -- // lmk if you wanna talk about adding or removing any attributes
 -- // - USER ID
@@ -43,6 +59,9 @@ expiry DATE,
 FOREIGN KEY (userID) REFERENCES users(id)
 );
 
+-- Testing SQL statments
+INSERT INTO reservations(userID, reg, lot, electric, accessibility, created_on, expiry) VALUES (18266401, '10LH1445', 'LOT A', 1, 0, DATE '2015-12-17', DATE '2015-12-17');
+
 --Ayoub - new transactions table
 -- id | userID | ReservationID | lot | amount | created_on
 CREATE TABLE IF NOT EXISTS transactions (
@@ -56,21 +75,5 @@ FOREIGN KEY (userID) REFERENCES users(id),
 FOREIGN KEY (reservationsID) REFERENCES reservations(id)
 );
 
-
-INSERT INTO users (id, firstName, LastName, password, email_address, status, electric, penalties, ban_status, accessibility, created_on, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", "222LH1445");
-
-INSERT INTO users (id, firstName, LastName, password, status, electric accessibility, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", "222LH1445");
-
-INSERT INTO users (id, firstName, LastName, password, status, electric, accessibility, created_on, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", DATE '2015-12-17', "222LH1445");
-
-INSERT INTO `users` (`id`, `firstName`, `lastName`, `password`, `email_address`, `status`, `electric`, `penalties`, `ban_status`, `banTime`, `accessibility`, `created_on`, `dob`, `reg`) VALUES ('', '', '', MD5(''), NULL, NULL, NULL, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, NULL, NULL);
-
-UPDATE users SET ban_status = 1, banTime = DATE '2000-03-12' WHERE id = "18266401"
-
-INSERT INTO reservations(userID, reg, lot, electric, accessibility, created_on, expiry) VALUES (18266401, '10LH1445', 'LOT A', 1, 0, DATE '2015-12-17', DATE '2015-12-17');
-
+-- Testing SQL statments
 INSERT INTO transactions(userID, reservationsID, lot, amount,created_on) VALUES (18266401, 1, "Lot A", 2.40,  DATE '2015-12-17');
-
-INSERT INTO users (id, firstName, LastName, password, status, electric, accessibility, dob, reg ) VALUES (18266401, "Ayoub", "Jdair", "Password123", "Student", 1, 0, "2000-12-15", "222LH1445");
-
-
