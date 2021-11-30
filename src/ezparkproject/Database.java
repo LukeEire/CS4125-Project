f8675436f7ba63ac3caed7f3422155834b105c92
LukeEire <18250785@studentmail.ul.ie>
2021-11-30 17:53:46 +0000

    Added fetchSingleReservation Method(Needs more testing)

--- src/ezparkproject/Database.java
+++ src/ezparkproject/Database.java
@@ -856,6 +856,37 @@ public class Database {
 			
 	}
 
+	//Pre-condition: Reservation with id must exist in DB
+		//Post-condition: Reservation object is returned 
+		public void fetchSingleReservation(int id) throws Exception{
+			try {
+				Reservation res = new Reservation();
+				String query = "select * from " + reservations_db + " where id = " + id;
+				PreparedStatement p = con.prepareStatement(query);
+				ResultSet rs = p.executeQuery(query);
+				int i = 1;
+				while(rs.next()){
+
+					System.out.println("Reservations ID: " + rs.getInt("id"));
+					System.out.println("User ID: " + rs.getInt("userID"));
+					System.out.println("Registration Plate: " + rs.getString("reg"));
+					System.out.println("Electric Car? [Y/N]: " + rs.getString("electric"));
+					System.out.println("Assistance Required? [Y/N]: " + rs.getString("accessibility"));
+					System.out.println("Reservation Date: " + rs.getTimestamp("created_on"));
+					System.out.println("Reserved until: " + rs.getTimestamp("expiry"));
+					System.out.println("-----------------------------END-------------------------------");
+					i++;
+					
+				}
+				
+			} catch (Exception e) {
+				
+				System.out.println("Error fetching data: " + e.getMessage());
+				
+			}
+				
+		}
+	
 	// Pre-Condition - User passed must exist in DB
 	// Post-Condition - Returns ArrayList of type Reservations with reservation collected from the DB
 	public ArrayList<Reservation> fetchUserReservation(Users user) throws Exception{
