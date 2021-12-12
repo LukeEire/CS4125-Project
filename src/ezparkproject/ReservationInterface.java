762fe553bf697d948b0ca0db5d516d9a3c36b1ec
LukeEire <18250785@studentmail.ul.ie>
2021-12-12 22:53:47 +0000

    Implemented factory method fully with Booking interface

---
+++ src/ezparkproject/ReservationInterface.java
@@ -0,0 +1,42 @@
+package ezparkproject;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+public interface ReservationInterface {
+	
+	Users getUser();
+	
+	void setUser(Users user);
+    
+    Date getcheckOutDate();
+
+    void setcheckOutDate(Date checkOutDate);
+
+    String getLot();
+    
+    void setLot(String lot);
+    
+    String getReg();
+    
+    void setReg(String reg);
+    
+    Long getHours();
+    
+    public void setHours(Long hours);
+    
+    int getChargingCheck();
+    
+    void setCharging(int elec);
+
+    void checkOut(Date checkOut);
+    
+    LocalDateTime getDuration();
+
+	void setDuration(LocalDateTime duration);
+
+	public int getId();
+
+	void setId(int id);
+	
+}
